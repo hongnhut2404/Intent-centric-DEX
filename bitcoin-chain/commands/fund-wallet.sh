4a2773ec44fd213ccec8987094464cfc60b7c4aa
hongnhut2404 <nhutth2404@gmail.com>
2025-06-10 14:24:27 +0700

    change directory in fund-wallet.sh

--- bitcoin-chain/commands/fund-wallet.sh
+++ bitcoin-chain/commands/fund-wallet.sh
@@ -1,24 +1,26 @@
-if [[ ! -f /home/nhutthi/Documents/bitcoin-28.1/mineraddress.txt ]]; then
-    echo "Error: mineraddress.txt not found in /home/nhutthi/Documents/bitcoin-28.1/"
+#!/bin/bash
+
+if [[ ! -f ./mineraddress.txt ]]; then
+    echo "Error: mineraddress.txt not found in $(pwd)"
     exit 1
 fi
 
-address=$(cat /home/nhutthi/Documents/bitcoin-28.1/mineraddress.txt)
-# cmd=$(grep "miner_address" /home/nhutthi/Documents/bitcoin-28.1/address.txt | cut -d'=' -f2)
-# 1. Generate 101 blocks to miner
-bitcoin-cli generatetoaddress 103 $address
-bitcoin-cli getwalletinfo
+address=$(cat ./mineraddress.txt)
 
-# 2. Get new block height
-initial_height=$(bitcoin-cli getblockcount)
+# 1. Generate 103 blocks to miner address
+./bin/bitcoin-cli generatetoaddress 103 "$address"
+./bin/bitcoin-cli getwalletinfo
+
+# 2. Get current block height
+initial_height=$(./bin/bitcoin-cli getblockcount)
 
 # 3. Fund sender wallet
-bitcoin-cli sendtoaddress bcrt1qh3py35f24dfgx0y6uaznfx66vhm88nh93qvpd0 100
+./bin/bitcoin-cli sendtoaddress bcrt1qh3py35f24dfgx0y6uaznfx66vhm88nh93qvpd0 100
 
-# 4. Wait for sender funding to be mined
+# 4. Wait for the next block to be mined
 echo "Waiting for the next block..."
 while true; do
-  current_height=$(bitcoin-cli getblockcount)
+  current_height=$(./bin/bitcoin-cli getblockcount)
   if [[ "$current_height" -gt "$initial_height" ]]; then
     echo "New block mined at height: $current_height"
     break
@@ -26,6 +28,7 @@ while true; do
   sleep 1
 done
 
-# 5. Now you can safely scan for UTXO
-bitcoin-cli scantxoutset start "[\"addr(bcrt1qh3py35f24dfgx0y6uaznfx66vhm88nh93qvpd0)\"]" > /home/nhutthi/Documents/bitcoin-28.1/data-script/utxo.json
-echo "send successfully"
+# 5. Scan for UTXOs and write to JSON file
+./bin/bitcoin-cli scantxoutset start "[\"addr(bcrt1qh3py35f24dfgx0y6uaznfx66vhm88nh93qvpd0)\"]" > ./data-script/utxo.json
+
+echo "Send successfully"
