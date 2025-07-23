499ca94b7bab27bbf94ba6ddd5a6da80b4a4a9cc
hongnhut2404 <nhutth2404@gmail.com>
2025-07-23 23:11:49 +0700

    add refund transaction

--- bitcoin-chain/src/htlc/create-refund/main.go
+++ bitcoin-chain/src/htlc/create-refund/main.go
@@ -1,72 +1,117 @@
 package main
 
 import (
+	"bytes"
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
 
 	"github.com/btcsuite/btcd/btcec/v2"
 	"github.com/btcsuite/btcd/btcutil"
 	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"github.com/btcsuite/btcd/rpcclient"
 	"github.com/btcsuite/btcd/txscript"
+	"github.com/btcsuite/btcd/wire"
+	"github.com/joho/godotenv"
 )
 
-// CreateRefundHTLCContract creates a P2SH address with a timelocked refund-only HTLC
-func CreateRefundHTLCContract(senderPubKeyHex string, locktime int64) (string, string, error) {
-	// Decode and validate sender public key
-	senderPubKeyBytes, err := hex.DecodeString(senderPubKeyHex)
-	if err != nil {
-		return "", "", fmt.Errorf("invalid sender pubkey hex: %w", err)
+func loadEnv() {
+	paths := []string{"../../.env", "../.env", "./.env", "../../../.env"}
+	for _, path := range paths {
+		if err := godotenv.Load(path); err == nil {
+			return
+		}
 	}
-	if len(senderPubKeyBytes) != 33 || (senderPubKeyBytes[0] != 0x02 && senderPubKeyBytes[0] != 0x03) {
-		return "", "", fmt.Errorf("senderPubKeyHex must be a 33-byte compressed public key")
-	}
-
-	// Validate locktime
-	if locktime < 0 || locktime > 0xFFFFFFFF {
-		return "", "", fmt.Errorf("locktime must be between 0 and 4294967295")
-	}
-
-	// Parse public key
-	senderPubKey, err := btcec.ParsePubKey(senderPubKeyBytes)
-	if err != nil {
-		return "", "", fmt.Errorf("failed to parse sender pubkey: %w", err)
-	}
-
-	// Build redeem script
-	builder := txscript.NewScriptBuilder().
-		AddInt64(locktime).                          // <locktime>
-		AddOp(txscript.OP_CHECKLOCKTIMEVERIFY).      // OP_CHECKLOCKTIMEVERIFY
-		AddOp(txscript.OP_DROP).                     // OP_DROP
-		AddData(senderPubKey.SerializeCompressed()). // <senderPubKey>
-		AddOp(txscript.OP_CHECKSIG)                  // OP_CHECKSIG
-
-	redeemScript, err := builder.Script()
-	if err != nil {
-		return "", "", fmt.Errorf("failed to build redeem script: %w", err)
-	}
-
-	// Hash to get P2SH address
-	scriptHash := btcutil.Hash160(redeemScript)
-	address, err := btcutil.NewAddressScriptHashFromHash(scriptHash, &chaincfg.RegressionNetParams)
-	if err != nil {
-		return "", "", fmt.Errorf("failed to create P2SH address: %w", err)
-	}
-
-	// Return P2SH address and redeem script in hex
-	return address.EncodeAddress(), hex.EncodeToString(redeemScript), nil
+	log.Fatal("Error loading .env from any known location")
 }
 
 func main() {
-	// Example input
-	senderPubKeyHex := "0203a1b2c3d4e5f67890aabbccddeeff00112233445566778899aabbccddeeff00" // Replace with actual pubkey
-	locktime := int64(1700000)                                                              // Example locktime: block height or UNIX timestamp
+	loadEnv()
 
-	address, redeemScriptHex, err := CreateRefundHTLCContract(senderPubKeyHex, locktime)
+	// Load environment variables
+	utxoPath := os.Getenv("UTXO_HTLC_JSON")
+	addressPath := os.Getenv("ADDRESS_TEST")
+	statePath := os.Getenv("STATE_PATH_HTLC")
+
+	// Load HTLC redeemScript
+	addrRaw, _ := ioutil.ReadFile(addressPath)
+	var addrData map[string][]map[string]string
+	_ = json.Unmarshal(addrRaw, &addrData)
+	redeemHex := addrData["HTLC"][0]["redeemScript"]
+	redeemScript, _ := hex.DecodeString(redeemHex)
+
+	// Load UTXO data
+	utxoRaw, _ := ioutil.ReadFile(utxoPath)
+	var utxoData map[string]interface{}
+	_ = json.Unmarshal(utxoRaw, &utxoData)
+	utxo := utxoData["unspents"].([]interface{})[0].(map[string]interface{})
+	txidStr := utxo["txid"].(string)
+	vout := uint32(utxo["vout"].(float64))
+	amount := btcutil.Amount(utxo["amount"].(float64) * 1e8)
+
+	// Load sender key and address
+	stateRaw, _ := ioutil.ReadFile(statePath)
+	var state map[string]interface{}
+	_ = json.Unmarshal(stateRaw, &state)
+	sender := state["alice"].(map[string]interface{})
+	privHex := sender["privkey"].(string)
+	privKey, _ := btcec.PrivKeyFromBytes(hexDecode(privHex))
+	address, _ := btcutil.DecodeAddress(sender["address"].(string), &chaincfg.RegressionNetParams)
+	pkScript, _ := txscript.PayToAddrScript(address)
+
+	// Build transaction
+	tx := wire.NewMsgTx(wire.TxVersion)
+	txHash, _ := chainhash.NewHashFromStr(txidStr)
+	txIn := wire.NewTxIn(wire.NewOutPoint(txHash, vout), nil, nil)
+	txIn.Sequence = 0 // For locktime to be respected
+	tx.TxIn = append(tx.TxIn, txIn)
+
+	fee := btcutil.Amount(500)
+	refundAmount := int64(amount - fee)
+	txOut := wire.NewTxOut(refundAmount, pkScript)
+	tx.TxOut = append(tx.TxOut, txOut)
+
+	// Set locktime
+	tx.LockTime = 300
+
+	// Generate signature
+	sig, _ := txscript.RawTxInSignature(tx, 0, redeemScript, txscript.SigHashAll, privKey)
+	builder := txscript.NewScriptBuilder()
+	builder.AddData(sig)
+	builder.AddInt64(0) // No preimage for refund
+	builder.AddData(redeemScript)
+	sigScript, _ := builder.Script()
+	tx.TxIn[0].SignatureScript = sigScript
+
+	// Serialize and broadcast
+	var buf bytes.Buffer
+	tx.Serialize(&buf)
+	rawTxHex := hex.EncodeToString(buf.Bytes())
+	fmt.Println("Refund Raw TX:", rawTxHex)
+
+	// Broadcast
+	client, err := rpcclient.New(&rpcclient.ConnConfig{
+		Host:         os.Getenv("RPC_HOST"),
+		User:         os.Getenv("RPC_USER"),
+		Pass:         os.Getenv("RPC_PASS"),
+		HTTPPostMode: true,
+		DisableTLS:   true,
+	}, nil)
 	if err != nil {
-		fmt.Printf("Error creating refund HTLC: %v\n", err)
-		return
+		log.Fatalf("RPC connection error: %v", err)
 	}
+	txid, err := client.SendRawTransaction(tx, false)
+	if err != nil {
+		log.Fatalf("Broadcast failed: %v", err)
+	}
+	fmt.Println("Refund TXID:", txid.String())
+}
 
-	fmt.Println("Refund HTLC P2SH Address:", address)
-	fmt.Println("Redeem Script (hex):", redeemScriptHex)
+func hexDecode(s string) []byte {
+	b, _ := hex.DecodeString(s)
+	return b
 }
